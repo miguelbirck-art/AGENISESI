4f9dd134fb5eb994792df8222b14b25bb9cfaf4d
Thomas Bayão <mebmmathias09@gmail.com>
2025-09-12 11:31:04 -0300

    Add files via upload

---
+++ agenisesi/src/main/java/com/senai/infoa/agenisesi/AgenisesiApplication.java
@@ -0,0 +1,13 @@
+package com.senai.infoa.agenisesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenisesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenisesiApplication.class, args);
+	}
+
+}
